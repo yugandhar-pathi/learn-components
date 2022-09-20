7155196b87d83a2dfddc9a39a6e93a3665516ee2
Yugandhar Pathi <yugandhar.pathi@hpe.com>
2022-09-20 00:16:53 -0700

    add unit tests for Button and Text component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 import {StyledButton} from './Button.styles'
 
-export const Button = ({label, primary, secondary, ...props}) => {
-    return <StyledButton  primary={primary} secondary={secondary} {...props}>{label}</StyledButton>
+export const Button = ({label, dataTestId, primary, secondary, ...props}) => {
+    return <StyledButton data-testid={dataTestId} primary={primary} secondary={secondary} {...props}>{label}</StyledButton>
 }
 
 Button.propTypes = {
+    dataTestId: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     primary: PropTypes.bool,
     secondary: PropTypes.bool,
--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,2 +1,20 @@
-import Button from './Button'
+import {render, screen} from '@testing-library/react'
+import { Button } from './Button'
 
+test('Button component', () => {
+    render(<Button label="test" dataTestId="test-button" />)
+
+    expect(screen.getByTestId('test-button').textContent).toBe("test")
+})
+
+test('Primary button Snapshot', () => {
+    render(<Button label="primary" primary dataTestId="test-button" />)
+
+    expect(screen.getByTestId('test-button')).toMatchSnapshot()
+})
+
+test('Secondary button Snapshot', () => {
+    render(<Button label="secondary" secondary dataTestId="test-button" />)
+
+    expect(screen.getByTestId('test-button')).toMatchSnapshot()
+})
