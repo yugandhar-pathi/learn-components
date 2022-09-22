0d2de6aac20939f328cea6b05c23480d93ec8e03
Kalyani <kalyanichow8@gmail.com>
2022-09-22 12:13:13 +0530

    Implemented react router

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,16 @@
+import { Input, Button, Column } from "../../components"
+import {useNavigate} from 'react-router-dom'
+
+export const Login = () => {
+    const navigate = useNavigate()
+    const LoginHandle = () =>{
+        navigate('/home')
+    }
+    return(
+        <Column>
+        <Input styleType="outlined" text="User Name :" type="text" placeholder="Enter Your User Name Here"/>
+        <Input styleType="outlined" text="Password :" type="password" placeholder="Enter Your Password Here"/>
+        <Button label="Login" primary onClick={LoginHandle}/>
+        </Column>
+    )
+}
