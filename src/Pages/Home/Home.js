4c08e9a0c1eced04a0fc939720f9bcb47e689c14
Kalyani <kalyanichow8@gmail.com>
2022-09-26 10:45:09 +0530

    Added redux to the application

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,13 @@
 import { Button } from "../../components"
 import { useNavigate } from "react-router-dom"
 import { useTranslation } from "react-i18next"
+import { useSelector } from "react-redux"
 
 export const Home = () =>{
     const navigate = useNavigate()
     const { t } = useTranslation()
+
+    const name = useSelector(state => state.userdetails.username)
 
     const LogoutHandle = () =>{
         navigate('/') 
@@ -12,6 +15,7 @@ export const Home = () =>{
     
     return(
         <>
+        <p>{`Welcome ${name}`}</p>
         <p>{t('Welcome_Home')}</p>
         <Button label={t('logout')} onClick={LogoutHandle}/>
         </>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,16 +1,30 @@
+import {useState} from 'react'
+import {useDispatch} from 'react-redux'
 import { Input, Button, Column } from "../../components"
 import {useNavigate} from 'react-router-dom'
 import { useTranslation } from "react-i18next"
+import {setName} from '../../LoginSlice'
+
 
 export const Login = () => {
+     
+    const [username, setuserName] = useState("")
+
     const { t } = useTranslation()
     const navigate = useNavigate()
+
+    const dispatch = useDispatch()
+
+    const userNameHandle = (evt) =>{
+        setuserName(evt.target.value)
+    }
     const LoginHandle = () =>{
+        dispatch(setName(username)) 
         navigate('/home')
     }
     return(
         <Column>
-        <Input styleType="outlined" text={t('username')} type="text" placeHolder={t('username_placeholder')}/>
+        <Input onChange={userNameHandle} styleType="outlined" text={t('username')} type="text" placeHolder={t('username_placeholder')}/>
         <Input styleType="outlined" text={t('password')} type="password" placeHolder={t('password_placeholder')}/>
         <Button label="Login" primary onClick={LoginHandle}/>
         </Column>
